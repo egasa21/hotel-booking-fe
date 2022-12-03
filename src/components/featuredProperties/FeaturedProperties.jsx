da27d3295224fe253c3e44fd25e588b4db8a6d47
Nurfadhil12 <nuurrff@gmail.com>
2022-12-04 03:12:34 +0700

    update error

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from "react";
 import './featuredProperties.css'
+// import fetch from ''
 import useFetch from '../../hooks/useFetch';
+import axios from "axios";
 
 const FeaturedProperties = () => {
-    const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
-    console.log(data)
+    const [data, setData] = useState()
+    const [loading, setLoading] = useState(true)
+    const fetch = useFetch();
+    // const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+    // console.log(data)
+    useEffect(() => {
+        getData();
+    }, []);
+    const getData = () => {
+        try {
+            const response = axios.get(`/hotels?featured=true&limit=4"`);
+            setData(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div className='fp'>
